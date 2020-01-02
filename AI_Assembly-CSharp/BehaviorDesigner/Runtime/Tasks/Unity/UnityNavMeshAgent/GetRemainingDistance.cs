c079f3376505c95b5552a576bfa154bb386ed26e
request-time-out <coldloc@126.com>
2020-01-03 00:37:46 +0800

    re-init

--- AI_Assembly-CSharp/BehaviorDesigner/Runtime/Tasks/Unity/UnityNavMeshAgent/GetRemainingDistance.cs
+++ AI_Assembly-CSharp/BehaviorDesigner/Runtime/Tasks/Unity/UnityNavMeshAgent/GetRemainingDistance.cs
@@ -2,7 +2,7 @@
 // Type: BehaviorDesigner.Runtime.Tasks.Unity.UnityNavMeshAgent.GetRemainingDistance
 // Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
 // MVID: 3FB45F06-6483-4AD8-97CB-A1C42CCDD6C3
-// Assembly location: E:\GAME\illusion_AI\PluginDev\反编译阅读\AI_Assembly-CSharp.dll
+// Assembly location: E:\GAME\illusion_AI\PluginDev\\AI_Assembly-CSharp.dll
 
 using UnityEngine;
 using UnityEngine.AI;
